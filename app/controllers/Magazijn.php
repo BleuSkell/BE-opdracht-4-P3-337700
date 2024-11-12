16938bf70d8221c08d0bb962f75dfa1ddd3b539e
BleuSkell <kyasowi1@gmail.com>
2024-11-12 14:26:26 +0100

    fixing data message error

--- app/controllers/Magazijn.php
+++ app/controllers/Magazijn.php
@@ -86,10 +86,9 @@ class Magazijn extends BaseController
             $data['messageVisibility'] = "flex";
             $data['dataRows'] = NULL;
 
-            // header('Refresh:3; url=' . URLROOT . '/Homepages/index');
-        } else if ($data['dataRows'] = NULL) {
-            $data['message'] = "test message";
-            $data['dataRows'] = NULL;
+            header('Refresh:3; url=' . URLROOT . '/Homepages/index');
+        } else if ($result == NULL) {
+            $data['message'] = "";
             // header('Refresh:4; url=' . URLROOT . '/magazijn/index');
         } else {
             $data['dataRows'] = $result;
