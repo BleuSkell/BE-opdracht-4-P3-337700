2361dcc08842e298c0a9dcc6e4f316c41692935c
BleuSkell <kyasowi1@gmail.com>
2024-12-10 19:13:35 +0100

    quick commit

--- app/db/StoredProcedures/leverancier/spInsertNewLevering.sql
+++ app/db/StoredProcedures/leverancier/spInsertNewLevering.sql
@@ -1,30 +1,5 @@
 DROP PROCEDURE IF EXISTS spInsertNewLevering;
 
--- DELIMITER //
-
--- CREATE PROCEDURE spInsertNewLevering(
---     IN pLeverancierId INT,
---     IN pProductId INT,
---     IN pAantal INT,
---     IN pDatumEerstVolgendeLevering DATE
--- )
--- BEGIN
---     INSERT INTO ProductPerLeverancier (
---         LeverancierId, 
---         ProductId, 
---         Aantal, 
---         DatumLevering
---     )
---     VALUES (
---         pLeverancierId, 
---         pProductId, 
---         pAantal, 
---         IFNULL(pDatumEerstVolgendeLevering, NOW())
---     );
--- END //
-
--- DELIMITER ;
-
 DELIMITER //
 
 CREATE PROCEDURE spInsertNewLevering(
