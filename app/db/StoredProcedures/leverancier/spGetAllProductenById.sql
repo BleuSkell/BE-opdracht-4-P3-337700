9087a6585f07d72995159f4902088543ffe24f89
BleuSkell <kyasowi1@gmail.com>
2024-12-10 20:38:53 +0100

    limiter for producten results added (THANK YOU CLAUDE!!!!)

--- app/db/StoredProcedures/leverancier/spGetAllProductenById.sql
+++ app/db/StoredProcedures/leverancier/spGetAllProductenById.sql
@@ -10,7 +10,7 @@
 ************************************************/
 
 -- Noem de database voor de stored procedure
-use `jamin_a`;
+USE `jamin_a`;
 
 -- Verwijder de bestaande stored procedure
 DROP PROCEDURE IF EXISTS spGetAllProductenById;
@@ -22,34 +22,77 @@ CREATE PROCEDURE spGetAllProductenById(
 )
 BEGIN
 
-    SELECT      LEV.Id                      AS LeverancierId
-                ,LEV.Naam                   AS LeverancierNaam
-                ,LEV.ContactPersoon         AS LeverancierContact
-                ,LEV.LeverancierNummer
-                ,LEV.Mobiel                 AS LeverancierMobiel
-                ,PROD.Id                    AS ProductId
-                ,PROD.Naam                  AS ProductNaam
-                ,MAG.Id                     AS MagazijnId
-                ,MAG.AantalAanwezig      
-                ,MAG.VerpakkingseenheidInKG AS VerpakkingsEenheid
-                ,PPL.Id                     AS ProductPerLeverancierId
-                ,PPL.DatumLevering          AS LaatsteLevering
+    -- Create a temporary table to store the most recent delivery for each product
+    CREATE TEMPORARY TABLE TempProductDeliveries AS (
+        SELECT 
+            PPL.ProductId,
+            MAX(PPL.DatumLevering) AS MostRecentDelivery
+        FROM 
+            ProductPerLeverancier AS PPL
+        WHERE 
+            PPL.LeverancierId = LeverancierId
+        GROUP BY 
+            PPL.ProductId
+    );
 
-    FROM        ProductPerLeverancier AS PPL
+    -- Select the results with the most recent delivery for each product
+    WITH RankedProducts AS (
+        SELECT 
+            LEV.Id                      AS LeverancierId
+            ,LEV.Naam                   AS LeverancierNaam
+            ,LEV.ContactPersoon         AS LeverancierContact
+            ,LEV.LeverancierNummer
+            ,LEV.Mobiel                 AS LeverancierMobiel
+            ,PROD.Id                    AS ProductId
+            ,PROD.Naam                  AS ProductNaam
+            ,MAG.Id                     AS MagazijnId
+            ,MAG.AantalAanwezig      
+            ,MAG.VerpakkingseenheidInKG AS VerpakkingsEenheid
+            ,PPL.Id                     AS ProductPerLeverancierId
+            ,PPL.DatumLevering          AS LaatsteLevering
+            ,ROW_NUMBER() OVER (PARTITION BY PROD.Naam ORDER BY PPL.DatumLevering DESC) AS RowNum
+        FROM 
+            ProductPerLeverancier AS PPL
 
-    INNER JOIN Product AS PROD
-            ON PROD.Id = PPL.ProductId
-            
-    INNER JOIN Leverancier AS LEV
-            ON LEV.Id = PPL.LeverancierId
+        INNER JOIN TempProductDeliveries AS TPD
+                ON PPL.ProductId = TPD.ProductId 
+                AND PPL.DatumLevering = TPD.MostRecentDelivery
 
-    INNER JOIN Magazijn AS MAG
-            ON MAG.Id = PROD.Id
+        INNER JOIN Product AS PROD
+                ON PROD.Id = PPL.ProductId
+                
+        INNER JOIN Leverancier AS LEV
+                ON LEV.Id = PPL.LeverancierId
 
-    WHERE   PPL.LeverancierId = LeverancierId     
-            
-    ORDER BY PPL.DatumLevering DESC;
+        INNER JOIN Magazijn AS MAG
+                ON MAG.Id = PROD.Id
 
+        WHERE 
+            PPL.LeverancierId = LeverancierId     
+    )
+
+    SELECT 
+        LeverancierId,
+        LeverancierNaam,
+        LeverancierContact,
+        LeverancierNummer,
+        LeverancierMobiel,
+        ProductId,
+        ProductNaam,
+        MagazijnId,
+        AantalAanwezig,
+        VerpakkingsEenheid,
+        ProductPerLeverancierId,
+        LaatsteLevering
+    FROM 
+        RankedProducts
+    WHERE 
+        RowNum = 1
+    ORDER BY 
+        LaatsteLevering DESC;
+
+    -- Drop the temporary table
+    DROP TEMPORARY TABLE IF EXISTS TempProductDeliveries;
 
 END //
 DELIMITER ;
